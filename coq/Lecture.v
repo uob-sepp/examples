c0b4f3ba8e7292282b47081de79799958d95439d
Michael B. Gale <github@michael-gale.co.uk>
2024-11-20 10:46:51 +0000

    Add completed Coq proofs

--- coq/Lecture.v
+++ coq/Lecture.v
@@ -11,13 +11,38 @@ Fixpoint add n m :=
 
 Theorem add_left_identity :
   forall n : Nat, add Zero n = n.
-
+Proof.
+intro n.
+simpl.
+reflexivity.
+Qed.
 
 Theorem add_right_identity :
   forall n : Nat, add n Zero = n.
-
+Proof.
+intro n.
+induction n.
+simpl.
+reflexivity.
+simpl.
+rewrite IHn.
+reflexivity.
+Qed.
 
 Theorem add_associativity :
   forall x y z : Nat, add x (add y z) = add (add x y) z.
+Proof.
+intros.
+induction x.
+simpl.
+reflexivity.
+simpl.
+rewrite IHx.
+reflexivity.
+Qed.
+
+Require Extraction.
+Extraction Language Haskell.
+Extraction add.
 
 
